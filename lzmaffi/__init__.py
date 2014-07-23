3794df4128b3107bf2264fc49f4e5a6ca136e64d
Tomer Chachamu <tomer.chachamu@gmail.com>
2014-07-23 14:18:49 +0200

    bump version

--- lzmaffi/__init__.py
+++ lzmaffi/__init__.py
@@ -36,7 +36,7 @@ _MODE_READ     = 1
 _MODE_READ_EOF = 2
 _MODE_WRITE    = 3
 
-__version__ = "0.2.1"
+__version__ = "0.2.2"
 
 def memoryview_tobytes(buffer):
     return buffer.tobytes() if isinstance(buffer, memoryview) else buffer
